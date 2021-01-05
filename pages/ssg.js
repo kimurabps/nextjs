16a80dc731164922ceef284c8a4513449628520c
kimurabps <38060535+kimurabps@users.noreply.github.com>
2021-01-05 13:21:14 +0900

    Create ssg.js

---
+++ pages/ssg.js
@@ -0,0 +1,13 @@
+import fetch from 'isomorphic-unfetch'
+
+export default function Ssg({ datetime }) {
+  return (
+    <h1>{datetime}</h1>
+  )
+}
+
+export const getStaticProps = async () => {
+  const res = await fetch('http://worldtimeapi.org/api/timezone/Asia/Tokyo')
+  const json = await res.json()
+  return { props: { datetime: json.datetime } }
+}
