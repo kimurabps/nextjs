ca6fd608604990eb0ec9500ae1144034226ba3b8
kimurabps <38060535+kimurabps@users.noreply.github.com>
2021-01-05 15:52:45 +0900

    Update ssg.js

--- pages/ssg.js
+++ pages/ssg.js
@@ -7,5 +7,5 @@ export default function Ssg({ datetime }) {
 export const getStaticProps = async () => {
   const res = await fetch('https://nextjs-xi-pied.vercel.app/api/date')
   const json = await res.json()
-  return { props: { datetime: json.datetime } }
+  return { props: { date: json.date } }
 }
