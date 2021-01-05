5809298b9a15c26cd03246f2916ae59771882644
kimurabps <38060535+kimurabps@users.noreply.github.com>
2021-01-05 15:48:35 +0900

    Update isr.js

--- pages/isr.js
+++ pages/isr.js
@@ -5,7 +5,7 @@ export default function Isr({ datetime }) {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch('/api/date')
+  const res = await fetch('https://nextjs-xi-pied.vercel.app/api/date')
   const json = await res.json()
   return {
     props: { datetime: json.datetime },
